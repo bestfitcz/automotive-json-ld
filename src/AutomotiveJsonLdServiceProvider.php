be1bbc44f3fd373f94c4bd938d7202db56be0727
David Tomsik <david.tomsik@bestfit.cz>
2025-06-25 22:26:05 +0200

    Everithing siplified

--- src/AutomotiveJsonLdServiceProvider.php
+++ src/AutomotiveJsonLdServiceProvider.php
@@ -2,24 +2,21 @@
 
 namespace Bestfitcz\AutomotiveJsonLd;
 
-use Bestfitcz\AutomotiveJsonLd\Commands\AutomotiveJsonLdCommand;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class AutomotiveJsonLdServiceProvider extends PackageServiceProvider
+class AutomotiveJsonLdServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
+    public function register(): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('automotive-json-ld')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_automotive_json_ld_table')
-            ->hasCommand(AutomotiveJsonLdCommand::class);
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/automotive-json-ld.php', 'automotive-json-ld'
+        );
+    }
+
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../config/automotive-json-ld.php' => config_path('automotive-json-ld.php'),
+        ], 'config');
     }
 }
